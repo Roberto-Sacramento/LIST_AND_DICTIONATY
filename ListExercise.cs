98c69e3cb8de2f766198199bb9c1f3e91a2bf7ae
Roberto-Sacramento <robert_os_@outlook.com>
2025-02-14 16:29:38 -0300

    implmented the following methods: Sum(), FindMax() and FindMin().

--- ListExercise.cs
+++ ListExercise.cs
@@ -1,25 +1,64 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-namespace LIST_AND_DICTIONATY
-{
-    public class ListExercise
+    namespace LIST_AND_DICTIONATY
     {
-        public static int SumLists(List<int> numbers)
+        public class ListExercise
         {
-             if (numbers == null)
+            //static when use in a method cannot be access a class as an instance reference
+            public static int SumLists(List<int> numbers)
+            {
+                if (numbers == null)
+            {
+                throw new ArgumentNullException("The list cannot be null.");
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+            }
+        
+        public static int FindMax(List<int> numbers)
         {
-            throw new ArgumentNullException("The list cannot be null.");
+            if(numbers == null ||numbers.Count == 0)
+            {
+                throw new ArgumentException("The list cannot be null.");
+            }
+            int max  = numbers[0];
+
+            for(int i = 1 ; i < numbers.Count ; i++)
+            {
+                if(numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
+        }
+       
+       public static int FindMin(List<int> numbers)
+       {
+        if(numbers == null || numbers.Count ==0)
+        {
+            throw new ArgumentException("The list cannot be null");
         }
 
-        int sum = 0;
-        foreach (int number in numbers)
+        int min = numbers[0];
+
+        for(int i = 1 ; i < numbers.Count ; i++)
         {
-            sum += number;
+            if(numbers[i]< min)
+            {
+                min = numbers[i];
+            }
         }
-        return sum;
-        }
+        return min;
+
+       }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,15 @@
 
 // 1. Create an instance of the ListOperations class:
 //ListExercise listExercise = new ListExercise();
-var listExercise = new ListExercise();
+//var listExercise = new ListExercise();
 // 2. Create a list of integers:
 List <int> myNumbers = new List<int> () {1, 2, 3, 4 ,5, 3};
  // 3. Call the methods on the instance, passing the list:
 int sum = ListExercise.SumLists(myNumbers);
 Console.WriteLine($"Sum:{sum}");
+
+int max = ListExercise.FindMax(myNumbers);
+Console.WriteLine($"Max number is: {max}");
+
+int min = ListExercise.FindMin(myNumbers);
+Console.WriteLine($"min number is: {min}");
